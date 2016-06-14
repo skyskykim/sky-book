5b78ecefd9bc634dcff7cf30dd6afe017353ba97
hanuel kim <skynaraioio@naver.com>
2016-06-15 02:17:02 +0900

    데이터 파싱2
    xml 파일
    url 2개 읽어와서 정렬

--- script/main.py
+++ script/main.py
@@ -1,58 +1,70 @@
 import urllib.request
 import xml.etree.ElementTree as etree
-
-
+import xml.etree.ElementTree as ET
 
 class GetData:
-
-    key ="f3m3RPXAk63BbSomWLgUxBxPusrwo891R5kEPyezn2AgYQYS7gsGUKK8j5prylR%2B%2FkegJnDYQCH1GNxLOocbsg%3D%3D"
-    url ="http://openapi.mpm.go.kr/openapi/service/RetrievePblinsttEmpmnInfoService/getClsfList"
-
-
-# 이거는 문화 뭐있는지 그거
-#  문화  key = "f3m3RPXAk63BbSomWLgUxBxPusrwo891R5kEPyezn2AgYQYS7gsGUKK8j5prylR%2B%2FkegJnDYQCH1GNxLOocbsg%3D%3D"
- #  문화 url = "http://www.culture.go.kr/openapi/rest/cultureartspaces?_wadl&type=xml"
-
-   # 이거는 곤충 인증키 #key = "f3m3RPXAk63BbSomWLgUxBxPusrwo891R5kEPyezn2AgYQYS7gsGUKK8j5prylR%2B%2FkegJnDYQCH1GNxLOocbsg%3D%3D"
-  #이거는 곤충 url #  url = "http://api.nature.go.kr/openapi/service/rest/KpniService/systemSearch?st=1&ServiceKey=f3m3RPXAk63BbSomWLgUxBxPusrwo891R5kEPyezn2AgYQYS7gsGUKK8j5prylR%2B%2FkegJnDYQCH1GNxLOocbsg%3D%3D&region=5%EC%9B%94&type=xml"
-    # url
-   # url = "http://api.nature.go.kr/openapi/service/rest/KpniService/systemSearch?ServiceKey=f3m3RPXAk63BbSomWLgUxBxPusrwo891R5kEPyezn2AgYQYS7gsGUKK8j5prylR%2B%2FkegJnDYQCH1GNxLOocbsg%3D%3D&region= 5%EC%9B%94&_type=xml"
-    # http://api.nature.go.kr/openapi/service/rest/KpniService/systemSearch?ServiceKey=f3m3RPXAk63BbSomWLgUxBxPusrwo891R5kEPyezn2AgYQYS7gsGUKK8j5prylR%2B%2FkegJnDYQCH1GNxLOocbsg%3D%3D&region= 5%EC%9B%94&_type=xml
-
+    key = "f3m3RPXAk63BbSomWLgUxBxPusrwo891R5kEPyezn2AgYQYS7gsGUKK8j5prylR%2B%2FkegJnDYQCH1GNxLOocbsg%3D%3D"
+    url = "http://data.ex.co.kr/exopenapi/business/frugalStationInfo?serviceKey=f3m3RPXAk63BbSomWLgUxBxPusrwo891R5kEPyezn2AgYQYS7gsGUKK8j5prylR%2B%2FkegJnDYQCH1GNxLOocbsg%3D%3D&type=xml"
+    url2 = "http://data.ex.co.kr/exopenapi/business/lpgServiceAreaInfo?serviceKey=f3m3RPXAk63BbSomWLgUxBxPusrwo891R5kEPyezn2AgYQYS7gsGUKK8j5prylR%2B%2FkegJnDYQCH1GNxLOocbsg%3D%3D&type=xml"
 
     def main(self):
+        self.tree = etree.parse("data.xml")
+        self.root = self.tree.getroot()
+        self.tree2 = etree.parse("data2.xml")
+        self.root2 = self.tree2.getroot()
 
+    def save(self):
         xml = urllib.request.urlopen(self.url).read()
         f = open("data.xml", "wb")
         f.write(xml)
         f.close()
-
-        #self.tree = etree.parse("data.xml")
-        #self.root = self.tree.getroot()
-
-
+################################################
+        xml2 = urllib.request.urlopen(self.url2).read()
+        f = open("data2.xml","wb")
+        f.write(xml2)
+        f.close()
         pass
 
-    def save(self):
+    def location_print(self):       # 위치만 나타내기 ㅋㅋㅋ
 
-
+        for a in self.root.iter("list"):
+            print()
+            print(("위치 :"),a.findtext("location"))
         pass
 
     def test_print(self):
+        print(" 1. 알뜰 주유소")
+        for a in self.root.iter("list"):
+            print(a.findtext("location"))
+            print(a.findtext("oilCompany"))
+            print(a.findtext("routeCode"))
+            print(a.findtext("routeName"))
+            print(a.findtext("serviceAreaCode"))
+            print(a.findtext("serviceAreaName"))
+            print("-----------------------------------")
 
-        # for a in self.root.iter("list"):
-        #     print(a.findtext("batchMenu"))
-        #     print(a.findtext("direction"))
-        #     print(a.findtext("routeCode"))
-        #     print(a.findtext("routeName"))
-        #     print(a.findtext("salePrice"))
-        #     print(a.findtext("serviceAreaCode"))
-        #     print(a.findtext("serviceAreaName"))
+        print("------------------------------------------------------------------------------------")
+        print("------------------------------------------------------------------------------------")
+        print("------------------------------------------------------------------------------------")
+        print("------------------------------------------------------------------------------------")
+
+        print(" 2. LPG 주유쇼")
+        for a in self.root2.iter("list"):
+            print(a.findtext("location"))
+            print(a.findtext("oilCompany"))
+            print(a.findtext("routeCode"))
+            print(a.findtext("routeName"))
+            print(a.findtext("serviceAreaCode"))
+            print(a.findtext("serviceAreaName"))
+            print("-----------------------------------")
 
 
 
-        pass
 
 g_data = GetData()
 
+g_data.save()
 g_data.main()
+
+#g_data.location_print() #위치만 나타내기
+g_data.test_print()
